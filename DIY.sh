0baf6195fefc2649bf938acc0d50887fb6a6e352
cielpy <beijiu572@gmail.com>
2020-06-25 17:20:48 +0800

    添加安装 luci-app-openclash

--- DIY.sh
+++ DIY.sh
@@ -10,7 +10,8 @@
 # Modify image size for OFFICAL OpenWrt source code
 # sed -i '567c $(Device/tplink-8mlzma)' target/linux/ar71xx/image/tiny-tp-link.mk
 # sed -i '238c CONFIG_ATH79_MACH_TL_WR841N_V9=y' target/linux/ar71xx/config-4.14
+
 # Add third-party package
-# git clone https://github.com/P3TERX/xxx package/xxx
-# ./scripts/feeds update -a
-# ./scripts/feeds install -a
+
+cd packages
+wget https://github.com/vernesong/OpenClash/releases/download/v0.39.2-beta/luci-app-openclash_0.39.2-beta_all.ipk
