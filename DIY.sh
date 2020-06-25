2505ded14f2e15c6e0c7647d54c557e0d01fe2bb
cielpy <beijiu572@gmail.com>
2020-06-25 18:48:05 +0800

    添加 log，再次尝试修复文件路径不对

--- DIY.sh
+++ DIY.sh
@@ -14,12 +14,16 @@
 work_dir=$(pwd)
 
 # Add third-party package
+
+echo "检查 files 文件夹是否存在"
 if [ ! -d "files" ]; then
+    echo "files 不存在，创建 files 文件夹"
     mkdir files
 fi
 
 mkdir -p files/etc/openclash/core/
 
+echo "下载 luci-app-openclash..."
 cd packages
 wget -q https://github.com/vernesong/OpenClash/releases/download/v0.39.2-beta/luci-app-openclash_0.39.2-beta_all.ipk
 
@@ -27,43 +31,56 @@ cd $work_dir
 
 mkdir openclash_tmp
 cd openclash_tmp
+
+echo "下载 clash..."
 #/etc/openclash/core/clash
 wget -q https://github.com/vernesong/OpenClash/releases/download/v0.39.2-beta/clash-linux-amd64.tar.gz
 mkdir clash_tmp
 tar -xzvf clash-linux-amd64.tar.gz -C ./clash_tmp
 cd ./clash_tmp
-ls | grep clash | xargs -I {} mv {} clash
+
+if [ ! -f "clash" ]; then
+    ls | grep clash | xargs -I {} mv {} clash
+fi
 chmod +x clash
 cp ./* "$work_dir/etc/openclash/core/"
 cd ..
 rm -rf clash_tmp
 rm clash-linux-amd64.tar.gz 
 
+echo "下载 clash_tun..."
 # /etc/openclash/core/clash_tun
 wget -q https://github.com/vernesong/OpenClash/releases/download/TUN-Premium/clash-linux-amd64-2020.06.15.gbf68156.gz
 mkdir clash_tmp
 tar -xzvf clash-linux-amd64-2020.06.15.gbf68156.gz -C ./clash_tmp
 cd ./clash_tmp
-ls | grep clash | xargs -I {} mv {} clash_tun
-chmod +x clash
+if [ ! -f "clash_tun" ]; then
+    ls | grep clash | xargs -I {} mv {} clash_tun
+fi
+chmod +x clash_tun
 cp ./* "$work_dir/etc/openclash/core/"
 cd ..
 rm -rf clash_tmp
 rm clash-linux-amd64-2020.06.15.gbf68156.gz
 
+echo "下载 clash_game"
 # /etc/openclash/core/clash_game
 wget -q https://github.com/vernesong/OpenClash/releases/download/TUN/clash-linux-amd64.tar.gz 
 mkdir clash_tmp
 tar -xzvf clash-linux-amd64.tar.gz -C ./clash_tmp
 cd ./clash_tmp
-ls | grep clash | xargs -I {} mv {} clash_game
-chmod +x clash
+if [ ! -f "clash_game" ]; then
+    ls | grep clash | xargs -I {} mv {} clash_game
+fi
+chmod +x clash_game
 cp ./* "$work_dir/etc/openclash/core/"
 cd ..
 rm -rf clash_tmp
 rm clash-linux-amd64.tar.gz 
 
+echo "查看下载结果"
 ls "$work_dir/etc/openclash/core/"
 
+echo "清理文件"
 cd ..
 rm -rf openclash_tmp
