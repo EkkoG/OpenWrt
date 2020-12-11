b02093b7dbe6652bcb40e999e40d134d0cf0b17e
cielpy <beijiu572@gmail.com>
2020-12-11 10:28:26 +0800

    更新固件，更新 openclash，修改默认 LAN IP

--- DIY.sh
+++ DIY.sh
@@ -6,7 +6,7 @@
 # Version:1.0
 #=================================================
 # Modify default Lan IP
-# sed -i 's/192.168.1.1/192.168.0.128/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.33.1/g' package/base-files/files/bin/config_generate
 # Modify image size for OFFICAL OpenWrt source code
 # sed -i '567c $(Device/tplink-8mlzma)' target/linux/ar71xx/image/tiny-tp-link.mk
 # sed -i '238c CONFIG_ATH79_MACH_TL_WR841N_V9=y' target/linux/ar71xx/config-4.14
@@ -25,7 +25,7 @@ mkdir -p files/etc/openclash/core/
 
 echo "下载 luci-app-openclash..."
 cd packages
-wget -q https://github.com/vernesong/OpenClash/releases/download/v0.39.2-beta/luci-app-openclash_0.39.2-beta_all.ipk
+wget -q https://github.com/vernesong/OpenClash/releases/download/v0.41.07-beta/luci-app-openclash_0.41.07-beta_all.ipk
 
 cd $work_dir
 
@@ -51,7 +51,7 @@ rm clash-linux-amd64.tar.gz
 echo "下载 clash_tun..."
 sudo -E apt-get -qq install gzip
 # /etc/openclash/core/clash_tun
-wget -q https://github.com/vernesong/OpenClash/releases/download/TUN-Premium/clash-linux-amd64-2020.06.15.gbf68156.gz
+wget -q https://github.com/vernesong/OpenClash/releases/download/TUN-Premium/clash-linux-amd64-2020.11.23.g4474306.gz
 mkdir clash_tmp
 gunzip -c clash-linux-amd64-2020.06.15.gbf68156.gz > ./clash_tmp/clash_tun
 cd ./clash_tmp
