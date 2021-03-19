05ec9da947fa41f08c9940d1f63c95e7a8e2a67d
cielpy <beijiu572@gmail.com>
2021-03-19 11:05:31 +0800

    use correct repo source and add smartdns and chinadns

--- DIY.sh
+++ DIY.sh
@@ -11,11 +11,11 @@
 # sed -i '567c $(Device/tplink-8mlzma)' target/linux/ar71xx/image/tiny-tp-link.mk
 # sed -i '238c CONFIG_ATH79_MACH_TL_WR841N_V9=y' target/linux/ar71xx/config-4.14
 
-echo "src/gz simonsmh https://github.com/simonsmh/openwrt-dist/raw/packages/rockchip/armv8/" >> ./repositories.conf
+echo "src/gz simonsmh https://github.com/simonsmh/openwrt-dist/raw/packages/x86/64/" >> ./repositories.conf
 
 
 # 最新版本查看 https://github.com/vernesong/OpenClash/releases
-LUCI_APP_OPENCLASH_VERSION=0.41.08-beta
+# LUCI_APP_OPENCLASH_VERSION=0.41.08-beta
 # 最新版本查看 https://github.com/vernesong/OpenClash/releases/tag/TUN-Premium
 CLASH_TUN_FILE_NAME=clash-linux-amd64-2020.11.23.g4474306.gz
 
