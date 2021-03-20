52b7c2824e58f92fde31af790ca196e6abdfb79b
cielpy <beijiu572@gmail.com>
2021-03-20 19:12:42 +0800

    refine code

--- DIY.sh
+++ DIY.sh
@@ -1,39 +1,18 @@
 #!/bin/bash
-#=================================================
-# https://github.com/lujimmy/openwrt-imagebuilder 
-# Description: DIY script
-# Author: lujimmy
-# Version:1.0
-#=================================================
-# Modify default Lan IP
-# sed -i 's/192.168.1.1/192.168.33.1/g' package/base-files/files/bin/config_generate
-# Modify image size for OFFICAL OpenWrt source code
-# sed -i '567c $(Device/tplink-8mlzma)' target/linux/ar71xx/image/tiny-tp-link.mk
-# sed -i '238c CONFIG_ATH79_MACH_TL_WR841N_V9=y' target/linux/ar71xx/config-4.14
 
 echo "src/gz simonsmh https://github.com/simonsmh/openwrt-dist/raw/packages/x86/64" >> ./repositories.conf
 
 
-# 最新版本查看 https://github.com/vernesong/OpenClash/releases
-# LUCI_APP_OPENCLASH_VERSION=0.41.08-beta
-# 最新版本查看 https://github.com/vernesong/OpenClash/releases/tag/TUN-Premium
-CLASH_TUN_FILE_NAME=clash-linux-amd64-2021.03.10.gz
+# https://github.com/Dreamacro/clash/releases/tag/premium
+CLASH_TUN_RELEASE_DATE=2021.03.10
+# https://github.com/comzyh/clash/releases
+CLASH_GAME_RELEASE_DATE=20210310
+# https://github.com/Dreamacro/clash/releases
+CLASH_VERSION=1.4.2
 
 work_dir=$(pwd)
 
 # Add third-party package
-
-echo "检查 files 文件夹是否存在"
-if [ ! -d "files" ]; then
-    echo "files 不存在，创建 files 文件夹"
-    mkdir files
-fi
-
-mkdir -p files/etc/openclash/core/
-
-# echo "下载 luci-app-openclash..."
-# cd packages
-# wget https://github.com/vernesong/OpenClash/releases/download/v${LUCI_APP_OPENCLASH_VERSION}/luci-app-openclash_${LUCI_APP_OPENCLASH_VERSION}_all.ipk
 
 cd $work_dir
 
@@ -42,9 +21,9 @@ cd openclash_tmp
 
 echo "下载 clash..."
 #/etc/openclash/core/clash
-wget https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-amd64.tar.gz
+wget https://github.com/Dreamacro/clash/releases/download/v${CLASH_VERSION}/clash-linux-amd64-v${CLASH_VERSION}.gz
 mkdir clash_tmp
-tar -xzvf clash-linux-amd64.tar.gz -C ./clash_tmp
+gunzip -c clash-linux-amd64-v${CLASH_VERSION}.gz > ./clash_tmp/clash
 cd ./clash_tmp
 
 if [ ! -f "clash" ]; then
@@ -54,14 +33,14 @@ chmod +x clash
 cp ./* "$work_dir/files/etc/openclash/core/"
 cd ..
 rm -rf clash_tmp
-rm clash-linux-amd64.tar.gz 
+rm clash-linux-amd64-v${CLASH_VERSION}.gz
 
 echo "下载 clash_tun..."
 sudo -E apt-get -qq install gzip
 # /etc/openclash/core/clash_tun
-wget -q https://github.com/vernesong/OpenClash/releases/download/TUN-Premium/${CLASH_TUN_FILE_NAME}
+wget -q https://github.com/Dreamacro/clash/releases/download/premium/clash-linux-amd64-${CLASH_TUN_RELEASE_DATE}.gz
 mkdir clash_tmp
-gunzip -c $CLASH_TUN_FILE_NAME > ./clash_tmp/clash_tun
+gunzip -c clash-linux-amd64-${CLASH_TUN_RELEASE_DATE}.gz > ./clash_tmp/clash_tun
 cd ./clash_tmp
 if [ ! -f "clash_tun" ]; then
     ls | grep clash | xargs -I {} mv {} clash_tun
@@ -70,13 +49,13 @@ chmod +x clash_tun
 cp ./* "$work_dir/files/etc/openclash/core/"
 cd ..
 rm -rf clash_tmp
-rm $CLASH_TUN_FILE_NAME
+rm clash-linux-amd64-${CLASH_TUN_RELEASE_DATE}.gz
 
 echo "下载 clash_game"
 # /etc/openclash/core/clash_game
-wget -q https://github.com/vernesong/OpenClash/releases/download/TUN/clash-linux-amd64.tar.gz 
+wget -q https://github.com/comzyh/clash/releases/download/${CLASH_GAME_RELEASE_DATE}/clash-linux-amd64-${CLASH_GAME_RELEASE_DATE}.gz 
 mkdir clash_tmp
-tar -xzvf clash-linux-amd64.tar.gz -C ./clash_tmp
+gunzip -c clash-linux-amd64-${CLASH_GAME_RELEASE_DATE}.gz  > ./clash_tmp/clash_game
 cd ./clash_tmp
 if [ ! -f "clash_game" ]; then
     ls | grep clash | xargs -I {} mv {} clash_game
@@ -85,7 +64,7 @@ chmod +x clash_game
 cp ./* "$work_dir/files/etc/openclash/core/"
 cd ..
 rm -rf clash_tmp
-rm clash-linux-amd64.tar.gz 
+rm clash-linux-amd64-${CLASH_GAME_RELEASE_DATE}.gz
 
 echo "查看下载结果"
 ls "$work_dir/files/etc/openclash/core/"
@@ -95,5 +74,3 @@ cd ..
 rm -rf openclash_tmp
 
 cat system-custom.tpl  | sed "s/CUSTOM_PPPOE_USERNAME/$CUSTOM_PPPOE_USERNAME/g" | sed "s/CUSTOM_PPPOE_PASSWORD/$CUSTOM_PPPOE_PASSWORD/g" | sed "s/CUSTOM_LAN_IP/$CUSTOM_LAN_IP/g" | sed "s~CUSTOM_CLASH_CONFIG_URL~$CUSTOM_CLASH_CONFIG_URL~g" >  files/etc/uci-defaults/system-custom
-
-cat  files/etc/uci-defaults/system-custom
