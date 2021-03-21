8ea58374085d731031d161576a711dedd5187487
cielpy <beijiu572@gmail.com>
2021-03-21 15:34:20 +0800

    update readme

--- run.sh
+++ run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 if [ "$1" = 'amd64_19' ]; then
-    docker-compose up imagebuilder_x86_64_19_07 &&  docker-compose rm -f
-elif [ "$1" = 'amd64_21']; then
-    docker-compose up imagebuilder_x86_64_21_02 &&  docker-compose rm -f
-elif [ "$1" = 'rockchip_21']; then
-    docker-compose up imagebuilder_rockchip_21_02 &&  docker-compose rm -f
+    docker-compose up imagebuilder_x86_64_19_07 
+elif [ "$1" = 'amd64_21' ]; then
+    docker-compose up imagebuilder_x86_64_21_02 
+elif [ "$1" = 'rockchip_21' ]; then
+    docker-compose up imagebuilder_rockchip_21_02 
 fi
+docker-compose rm -f
