c2fa95f2ccc53c31cc6937fa4ec085c554f794d3
cielpy <beijiu572@gmail.com>
2021-03-21 14:35:38 +0800

    download ipks, not use local ipk

--- DIY.sh
+++ DIY.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# ARCH 可选 amd64 和 armv8
+ARCH=amd64
+### ================================================================
+
+
+
+
+
+
 echo "src/gz simonsmh https://github.com/simonsmh/openwrt-dist/raw/packages/x86/64" >> ./repositories.conf
 
 
@@ -9,8 +18,6 @@ CLASH_TUN_RELEASE_DATE=2021.03.10
 CLASH_GAME_RELEASE_DATE=20210310
 # https://github.com/Dreamacro/clash/releases
 CLASH_VERSION=1.4.2
-
-ARCH=amd64
 
 work_dir=$(pwd)
 
@@ -43,5 +50,20 @@ cd $work_dir
 
 echo "查看下载结果"
 ls "$work_dir/files/etc/openclash/core/"
+
+function download_missing_ipks() {
+    local url=$1
+    local binaryname=$2
+    wget $url
+    
+    local filename="${url##*/}"
+
+    cp $filename ./packages/$filename
+    rm $filename
+}
+
+download_missing_ipks https://downloads.openwrt.org/releases/packages-21.02/x86_64/packages/libcap_2.43-1_x86_64.ipk
+download_missing_ipks https://downloads.openwrt.org/releases/packages-21.02/x86_64/packages/libcap-bin_2.43-1_x86_64.ipk
+
 
 cat system-custom.tpl  | sed "s/CUSTOM_PPPOE_USERNAME/$CUSTOM_PPPOE_USERNAME/g" | sed "s/CUSTOM_PPPOE_PASSWORD/$CUSTOM_PPPOE_PASSWORD/g" | sed "s/CUSTOM_LAN_IP/$CUSTOM_LAN_IP/g" | sed "s~CUSTOM_CLASH_CONFIG_URL~$CUSTOM_CLASH_CONFIG_URL~g" >  files/etc/uci-defaults/system-custom
