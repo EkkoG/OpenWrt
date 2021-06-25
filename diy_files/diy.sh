571cc2430509f089faec43c72756c604f6222021
cielpy <beijiu572@gmail.com>
2021-06-26 00:02:02 +0800

    make r2s work

--- diy_files/diy.sh
+++ diy_files/diy.sh
@@ -11,7 +11,7 @@ if [ "$TARGET" = "x86_64" ]; then
 elif [ "$TARGET" = "rockchip" ]; then
     ARCH=armv8
     CUSTOM_IPK_ARCH=aarch64_generic
-    CUSTOM_SOURCE_ARCH="aarch64_generic/"
+    CUSTOM_SOURCE_ARCH="aarch64_generic"
 elif [ "$TARGET" = "ar71xx_nand" ]; then
     ARCH=mips-softfloat
     CUSTOM_IPK_ARCH=mips_24kc
@@ -22,8 +22,6 @@ elif [ "$TARGET" = "ar71xx_nand" ]; then
 
 fi
 
-# echo "src/gz opsupes https://op.supes.top/packages/${CUSTOM_IPK_ARCH}" >> ./repositories.conf
-echo "src/gz simonsmh https://github.com/simonsmh/openwrt-dist/raw/packages/${CUSTOM_SOURCE_ARCH}" >> ./repositories.conf
 echo "src/gz passwall https://gh-proxy.imciel.com/https://github.com/${PASSWALL_SOURCE}/blob/19.07.7/packages/${CUSTOM_SOURCE_ARCH}" >> ./repositories.conf
 
 if [ "$OPENWRT_VERSION" = "21.02" ]; then
@@ -37,7 +35,7 @@ if [ -f "diy_files/personal_diy/$FLAG.sh" ];then
     cat "diy_files/personal_diy/$FLAG.sh" >> /tmp/init.sh
     printf "\n" >> /tmp/init.sh
 fi
-cat "diy_files/$TARGET.sh" >> /tmp/init.sh
+cat "diy_files/common.sh" >> /tmp/init.sh
 printf "\n" >> /tmp/init.sh
 if [ "$WAN_TYPE" = "pppoe" ]; then
     cat "diy_files/pppoe.sh" >> /tmp/init.sh
@@ -171,4 +169,4 @@ fi
 download_missing_ipks https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.5/luci-theme-argon_2.2.5-20200914_all.ipk
 download_missing_ipks https://github.com/cielpy/luci-app-jd-dailybonus/releases/download/v1.0.5/luci-app-jd-dailybonus_1.0.5-20210316_all.ipk
 download_missing_ipks https://github.com/cielpy/luci-app-clash/releases/download/v1.8.1/luci-app-clash_v1.8.1_all.ipk
-download_missing_ipks https://github.com/cielpy/luci-app-clash/releases/download/v1.8.1/overture_1.7-1_${CUSTOM_IPK_ARCH}.ipk
+download_missing_ipks https://github.com/cielpy/luci-app-clash/releases/download/v1.8.1/overture_1.8-rc1-1_${CUSTOM_IPK_ARCH}.ipk
