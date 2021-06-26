ba36e3c08d664af13e2769163a506b14f4cbd811
cielpy <beijiu572@gmail.com>
2021-06-26 22:24:05 +0800

    refine code

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### 介绍
 
-自用 OpenWrt-x86_64/R2S 镜像
+自用 OpenWrt x86_64/R2S/WNDR4300 镜像
 
 ### 项目目标
 
@@ -8,20 +8,34 @@
 
 ### 特色
 
-- 采用官方原版 ImageBuilder 构建，可按照官方提供的方式进行自定义，x86_64 支持 19.07 和 21.02 两个版本，R2S 支持 21.02 一个版本
-- 集成 [OpenClash](https://github.com/vernesong/OpenClash), [China-DNS](https://github.com/aa65535/openwrt-chinadns), [SmartDNS](https://github.com/pymumu/smartdns/)
-- 默认使用 OpenClash+SmartDNS 组合，两个组件间的联动配置已配置好，构建固件时只需要提供 clash 配置链接即可在刷机完成后，直接启动 OpenClash
-- 可配置默认 LAN 口 IP，PPPoE 账号密码，刷机完成后，几乎不用任何配置即可使用
-- 可以自定义，根据 [UCI](https://openwrt.org/docs/guide-user/base-system/uci) 语法，可以自定义几乎所有配置，只需要将 uci 命令录入 `uci-diy.tpl.sh` 即可，uci-diy.tpl.sh 也可以酌情删减，**删除时请务必清楚删除的配置的作用！！！**
+- 采用官方原版 ImageBuilder 构建，可按照官方提供的方式进行自定义，无代码修改，版本支持见
+- 集成常用软件，软件列表见
+- 默认使用 Clash+Overture组合，两个组件间的联动配置已配置好，构建固件时只需要提供 clash 配置链接即可在刷机完成后，直接启动 OpenClash
+- 可配置默认 LAN 口 IP，PPPoE 账号密码，刷机完成后，不用配置网络
+- 可以自定义，根据 [UCI](https://openwrt.org/docs/guide-user/base-system/uci) 语法，可以自定义几乎所有配置，只需要将 uci 命令录入 `diy_files/diy.sh` 即可，diy.sh 也可以酌情删减，**删除时请务必清楚删除的配置的作用！！！**
 
+#### 版本支持情况
+
+|  设备   | 19.07  | 21.02 |
+|  ----  | ----  | ---- |
+| x86_64  | ✅ | ✅ |
+| R2S | ❌ | ✅ |
+| WNDR4300 | ✅ | ❌ |
+
+#### 软件列表
+
+- https://github.com/jerrykuku/luci-app-jd-dailybonus
+- https://github.com/frainzy1477/luci-app-clash
+- https://github.com/EXSERENS/overture-openwrt
+- https://github.com/jerrykuku/luci-theme-argon
 
 ### 使用
 
 - git clone https://github.com/cielpy/OpenWrt.git
 - cd OpenWrt
 - 新建 .env 文件，配置环境变量，请参考 https://github.com/cielpy/OpenWrt#%E6%94%AF%E6%8C%81%E9%85%8D%E7%BD%AE%E7%9A%84%E7%8E%AF%E5%A2%83%E5%8F%98%E9%87%8F
-- 修改 uci-diy.tpl.sh（非必须，修改须谨慎）
-- ./run.sh rockchip_21/amd64_21/amd64_19 .env
+- 修改 diy_files/diy.sh（非必须，修改须谨慎）
+- ./run.sh rockchip_21/amd64_21/amd64_19
 
 
 ### 支持配置的环境变量
@@ -31,6 +45,9 @@ CUSTOM_PPPOE_PASSWORD=
 CUSTOM_PPPOE_USERNAME=
 CUSTOM_LAN_IP=
 CUSTOM_CLASH_CONFIG_URL=
+CUSTOM_PASSWALL_SUBSCRIBE_URL=
+WAN_TYPE=
+FLAG=
 ```
 
 感谢以下项目，使得我的上网体验又有所提升
--- diy_files/diy.sh
+++ diy_files/diy.sh
@@ -1,68 +1,7 @@
 #!/bin/bash -e
 
-CUSTOM_SOURCE_ARCH=
 CUSTOM_IPK_ARCH=
-ARCH=
-
-if [ "$TARGET" = "x86_64" ]; then
-    CUSTOM_IPK_ARCH=x86_64
-    CUSTOM_SOURCE_ARCH="x86/64"
-    ARCH=amd64
-elif [ "$TARGET" = "rockchip" ]; then
-    ARCH=armv8
-    CUSTOM_IPK_ARCH=aarch64_generic
-    CUSTOM_SOURCE_ARCH="aarch64_generic"
-elif [ "$TARGET" = "ar71xx_nand" ]; then
-    ARCH=mips-softfloat
-    CUSTOM_IPK_ARCH=mips_24kc
-    CUSTOM_SOURCE_ARCH="ar71xx/generic"
-
-    mkdir -p files/etc/config
-    cp diy_files/config/wireless files/etc/config/wireless
-
-fi
-
-echo "src/gz passwall https://op.supes.top/packages/${CUSTOM_SOURCE_ARCH}" >> ./repositories.conf
-if grep 'check_signature' ./repositories.conf
-then
-    sed -i '/check_signature/d' ./repositories.conf
-fi
-
-if [ "$OPENWRT_VERSION" = "21.02" ]; then
-    echo "src imagebuilder file:packages" >> ./repositories.conf
-fi
-
-mkdir -p files/etc/uci-defaults/
-cat diy_files/uci-diy.tpl.sh > /tmp/init.sh
-printf "\n" >> /tmp/init.sh
-if [ -f "diy_files/personal_diy/$FLAG.sh" ];then
-    cat "diy_files/personal_diy/$FLAG.sh" >> /tmp/init.sh
-    printf "\n" >> /tmp/init.sh
-fi
-cat "diy_files/common.sh" >> /tmp/init.sh
-printf "\n" >> /tmp/init.sh
-if [ "$WAN_TYPE" = "pppoe" ]; then
-    cat "diy_files/pppoe.sh" >> /tmp/init.sh
-    printf "\n" >> /tmp/init.sh
-fi
-
-cat /tmp/init.sh | \
- sed "s/CUSTOM_PPPOE_USERNAME/$CUSTOM_PPPOE_USERNAME/g" | \
- sed "s/CUSTOM_PPPOE_PASSWORD/$CUSTOM_PPPOE_PASSWORD/g" | \
- sed "s/CUSTOM_LAN_IP/$CUSTOM_LAN_IP/g" | \
- sed "s~CUSTOM_CLASH_CONFIG_URL~$CUSTOM_CLASH_CONFIG_URL~g" | \
- sed "s~CUSTOM_PASSWALL_SUBSCRIBE_URL~$CUSTOM_PASSWALL_SUBSCRIBE_URL~g" \
- >  files/etc/uci-defaults/uci-diy
-
-if [ -f "diy_files/personal_diy/$FLAG.pub" ];then
-    mkdir -p files/etc/dropbear/
-    cat "diy_files/personal_diy/$FLAG.pub" >> files/etc/dropbear/authorized_keys
-    chmod 644 files/etc/dropbear/authorized_keys
-fi
-
-if [ -d keys ]; then
-    cp diy_files/keys/* keys
-fi
+CLASH_ARCH=
 
 # https://github.com/Dreamacro/clash/releases/tag/premium
 CLASH_TUN_RELEASE_DATE=2021.05.08
@@ -71,15 +10,23 @@ CLASH_GAME_RELEASE_DATE=20210310
 # https://github.com/Dreamacro/clash/releases
 CLASH_VERSION=1.6.0
 
-work_dir=$(pwd)
 
-# Add third-party package
+if [ "$TARGET" = "x86_64" ]; then
+    CUSTOM_IPK_ARCH=x86_64
+    CLASH_ARCH=amd64
+elif [ "$TARGET" = "rockchip" ]; then
+    CUSTOM_IPK_ARCH=aarch64_generic
+    CLASH_ARCH=armv8
+elif [ "$TARGET" = "ar71xx_nand" ]; then
+    CUSTOM_IPK_ARCH=mips_24kc
+    CLASH_ARCH=mips-softfloat
 
+    mkdir -p files/etc/config
+    cp diy_files/config/wireless files/etc/config/wireless
 
-cd $work_dir
+fi
 
-sudo -E apt-get -qq install gzip
-
+# 工具方法定义
 function download_clash_binaries() {
     local url=$1
     local binaryname=$2
@@ -132,27 +79,6 @@ function add_anti_ad() {
     popd
 }
 
-add_anti_ad
-# rm -rf *
-
-
-if [ "$TARGET" != "ar71xx_nand" ]; then
-    mkdir -p files/etc/clash/
-    pushd files/etc/clash/
-    download_clash_binaries https://github.com/cielpy/clash/releases/download/v${CLASH_VERSION}/clash-linux-${ARCH}-v${CLASH_VERSION}.gz clash
-# if [ "$TARGET" != "ar71xx_nand" ]; then
-#     download_clash_binaries https://github.com/Dreamacro/clash/releases/download/premium/clash-linux-${ARCH}-${CLASH_TUN_RELEASE_DATE}.gz clash ./dtun/
-#     download_clash_binaries https://github.com/comzyh/clash/releases/download/${CLASH_GAME_RELEASE_DATE}/clash-linux-${ARCH}-${CLASH_GAME_RELEASE_DATE}.gz clash ./clashtun/
-# fi
-    popd
-fi
-
-if [ "$TARGET" = "ar71xx_nand" ]; then
-    download_xray
-fi
-
-cd $work_dir
-
 function download_missing_ipks() {
     local url=$1
     local binaryname=$2
@@ -165,12 +91,94 @@ function download_missing_ipks() {
     rm $filename
 }
 
+# =================================================================
 
+# 添加软件源
+echo "src/gz supres https://op.supes.top/packages/${CUSTOM_IPK_ARCH}" >> ./repositories.conf
+if grep 'check_signature' ./repositories.conf
+then
+    sed -i '/check_signature/d' ./repositories.conf
+fi
+
+if [ "$OPENWRT_VERSION" = "21.02" ]; then
+    echo "src imagebuilder file:packages" >> ./repositories.conf
+fi
+
+# 如果需要，添加签名验证的 key
+if [ -d keys ] && [ -d diy_files/keys ]; then
+    cp diy_files/keys/* keys
+fi
+
+
+# 添加自定义 uci 脚本
+mkdir -p files/etc/uci-defaults/
+cat diy_files/uci-diy.tpl.sh > /tmp/init.sh
+printf "\n" >> /tmp/init.sh
+
+# 针对个人的自定义，需要在定义 FLAG 环境变量，并在 diy_files/personal_diy 下放一个脚本，脚本名为 $FLAG.sh
+if [ -f "diy_files/personal_diy/$FLAG.sh" ];then
+    cat "diy_files/personal_diy/$FLAG.sh" >> /tmp/init.sh
+    printf "\n" >> /tmp/init.sh
+fi
+cat "diy_files/common.sh" >> /tmp/init.sh
+printf "\n" >> /tmp/init.sh
+
+# 如果上网方法为 pppoe，记得在 diy_files 下放一个 pppoe.sh, 定义网络配置命令
+if [ "$WAN_TYPE" = "pppoe" ]; then
+    cat "diy_files/pppoe.sh" >> /tmp/init.sh
+    printf "\n" >> /tmp/init.sh
+fi
+
+# 替换自定义参数
+cat /tmp/init.sh | \
+ sed "s/CUSTOM_PPPOE_USERNAME/$CUSTOM_PPPOE_USERNAME/g" | \
+ sed "s/CUSTOM_PPPOE_PASSWORD/$CUSTOM_PPPOE_PASSWORD/g" | \
+ sed "s/CUSTOM_LAN_IP/$CUSTOM_LAN_IP/g" | \
+ sed "s~CUSTOM_CLASH_CONFIG_URL~$CUSTOM_CLASH_CONFIG_URL~g" | \
+ sed "s~CUSTOM_PASSWALL_SUBSCRIBE_URL~$CUSTOM_PASSWALL_SUBSCRIBE_URL~g" \
+ >  files/etc/uci-defaults/uci-diy
+
+# 添加 SSH key，放在 diy_files/personal_diy 下，名字为 $FLAG.pub
+if [ -f "diy_files/personal_diy/$FLAG.pub" ];then
+    mkdir -p files/etc/dropbear/
+    cat "diy_files/personal_diy/$FLAG.pub" >> files/etc/dropbear/authorized_keys
+    chmod 644 files/etc/dropbear/authorized_keys
+fi
+
+# 安装依赖
+sudo -E apt-get -qq install gzip
+
+# 添加去广告
+add_anti_ad
+
+# 添加 clash 可执行文件，ar71xx_nand 上由于空间太小，不适合使用 clash
+if [ "$TARGET" != "ar71xx_nand" ]; then
+    mkdir -p files/etc/clash/
+    pushd files/etc/clash/
+    download_clash_binaries https://github.com/cielpy/clash/releases/download/v${CLASH_VERSION}/clash-linux-${CLASH_ARCH}-v${CLASH_VERSION}.gz clash
+# if [ "$TARGET" != "ar71xx_nand" ]; then
+#     download_clash_binaries https://github.com/Dreamacro/clash/releases/download/premium/clash-linux-${CLASH_ARCH}-${CLASH_TUN_RELEASE_DATE}.gz clash ./dtun/
+#     download_clash_binaries https://github.com/comzyh/clash/releases/download/${CLASH_GAME_RELEASE_DATE}/clash-linux-${CLASH_ARCH}-${CLASH_GAME_RELEASE_DATE}.gz clash ./clashtun/
+# fi
+    popd
+fi
+
+# 在 ar71xx_nand 上添加 xray 可执行文件，其他平台安装 luci-app-passwall 时会自动安装 xray
+if [ "$TARGET" = "ar71xx_nand" ]; then
+    download_xray
+fi
+
+# libcap-bin 在 19.07 上缺失，需要单独添加安装包
 if [ "$OPENWRT_VERSION" = "19.07" ]; then
     download_missing_ipks https://downloads.openwrt.org/releases/packages-21.02/${CUSTOM_IPK_ARCH}/packages/libcap_2.43-1_${CUSTOM_IPK_ARCH}.ipk
     download_missing_ipks https://downloads.openwrt.org/releases/packages-21.02/${CUSTOM_IPK_ARCH}/packages/libcap-bin_2.43-1_${CUSTOM_IPK_ARCH}.ipk
 fi
+
+# 添加自定义主题
 download_missing_ipks https://github.com/jerrykuku/luci-theme-argon/releases/download/v2.2.5/luci-theme-argon_2.2.5-20200914_all.ipk
+# 添加京东签到
 download_missing_ipks https://github.com/cielpy/luci-app-jd-dailybonus/releases/download/v1.0.5/luci-app-jd-dailybonus_1.0.5-20210316_all.ipk
+# 添加自定义的 luci-app-clash
 download_missing_ipks https://github.com/cielpy/luci-app-clash/releases/download/v1.9.0/luci-app-clash_v1.9.0_all.ipk
+# 添加自定义的 overture
 download_missing_ipks https://github.com/cielpy/overture-openwrt/releases/download/v1.8-rc1/overture_1.8-rc1-1_${CUSTOM_IPK_ARCH}.ipk
