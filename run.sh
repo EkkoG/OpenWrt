b177aa7cd4ce89f928a14faa87d839cfffaa8455
EkkoG <beijiu572@gmail.com>
2022-10-24 23:53:12 +0800

    remove 19.07 support, add 22.03 support

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [ "$1" = 'amd64_19' ]; then
-    docker-compose up imagebuilder_x86_64_19_07
+if [ "$1" = 'amd64_22' ]; then
+    docker-compose up imagebuilder_x86_64_22_03
 elif [ "$1" = 'amd64_21' ]; then
     docker-compose up imagebuilder_x86_64_21_02
 elif [ "$1" = 'rockchip_21' ]; then
     docker-compose up imagebuilder_rockchip_21_02
+elif [ "$1" = 'rockchip_22' ]; then
+    docker-compose up imagebuilder_rockchip_22_03
 fi
 docker-compose rm -f
