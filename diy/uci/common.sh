752cb36eece01b5c2ccb14a76bb75f7708ce99f8
EkkoG <beijiu572@gmail.com>
2022-11-02 23:41:25 +0800

    Revert "use uci batch syntax"
    This reverts commit a93bc3d7a15d0002557792f8c3f9436b7fe7efe4.

--- diy/uci/common.sh
+++ diy/uci/common.sh
@@ -1,62 +1,65 @@
 
+uci set overture.@overture[0].VerboseMode='0'
+uci set overture.@overture[0].LogToFile='/var/overture.log'
+uci commit overture
+
+
+uci set system.@system[0].zonename='Asia/Shanghai'
+uci set system.@system[0].timezone='CST-8'
+uci set system.@system[0].cronloglevel='8'
+uci set system.@system[0].conloglevel='7'
+uci commit system
+
+uci set luci.main.lang='zh_cn'
+uci commit luci
+
 while uci -q delete openclash.@dns_servers[0]; do :; done
+uci add openclash dns_servers
+uci set openclash.@dns_servers[-1]=dns_servers
+uci set openclash.@dns_servers[-1].group='nameserver'
+uci set openclash.@dns_servers[-1].type='udp'
+uci set openclash.@dns_servers[-1].enabled='1'
+uci set openclash.@dns_servers[-1].ip='127.0.0.1'
+uci set openclash.@dns_servers[-1].port='7053'
+
 while uci -q delete openclash.@config_subscribe[0]; do :; done
+uci add openclash config_subscribe
+uci set openclash.@config_subscribe[-1]=config_subscribe
+uci set openclash.@config_subscribe[-1].enabled='1'
+uci set openclash.@config_subscribe[-1].address='CLASH_CONFIG_URL'
+uci set openclash.@config_subscribe[-1].sub_convert='0'
 
 # 默认 SOCKS 不需要认证
 while uci -q delete openclash.@authentication[0]; do :; done
 
-uci batch << EOF
-set overture.@overture[0].VerboseMode='0'
-set overture.@overture[0].LogToFile='/var/overture.log'
+uci set openclash.config.ipv6_enable='1'
+uci set openclash.config.china_ip6_route='1'
+uci set openclash.config.china_ip_route='1'
+uci set openclash.config.geo_auto_update='1'
+uci set openclash.config.geo_update_day_time='10'
+uci set openclash.config.chnr_auto_update='1'
+uci set openclash.config.auto_restart='1'
+uci set openclash.config.auto_restart_day_time='3'
+uci set openclash.config.enable_dns='1'
+uci set openclash.config.ipv6_dns='1'
+uci set openclash.config.append_wan_dns='0'
+uci set openclash.config.disable_udp_quic='0'
+uci set openclash.config.auto_update='1'
+uci set openclash.config.config_auto_update_mode='0'
+uci set openclash.config.config_update_week_time='*'
 
-set system.@system[0].zonename='Asia/Shanghai'
-set system.@system[0].timezone='CST-8'
-set system.@system[0].cronloglevel='8'
-set system.@system[0].conloglevel='7'
+uci set openclash.config.core_type='Meta'
+uci set openclash.config.enable_meta_core='1'
+uci set openclash.config.enable_meta_sniffer='1'
+uci set openclash.config.enable_meta_sniffer_custom='0'
+uci set openclash.config.common_ports='1'
+uci set openclash.config.append_default_dns '0'
+uci set openclash.config.enable_custom_dns '1'
 
-set luci.main.lang='zh_cn'
+uci commit openclash
 
-uci add openclash dns_servers
-set openclash.@dns_servers[-1]=dns_servers
-set openclash.@dns_servers[-1].group='nameserver'
-set openclash.@dns_servers[-1].type='udp'
-set openclash.@dns_servers[-1].enabled='1'
-set openclash.@dns_servers[-1].ip='127.0.0.1'
-set openclash.@dns_servers[-1].port='7053'
-
-uci add openclash config_subscribe
-set openclash.@config_subscribe[-1]=config_subscribe
-set openclash.@config_subscribe[-1].enabled='1'
-set openclash.@config_subscribe[-1].address='CLASH_CONFIG_URL'
-set openclash.@config_subscribe[-1].sub_convert='0'
-
-set openclash.config.ipv6_enable='1'
-set openclash.config.china_ip6_route='1'
-set openclash.config.china_ip_route='1'
-set openclash.config.geo_auto_update='1'
-set openclash.config.geo_update_day_time='10'
-set openclash.config.chnr_auto_update='1'
-set openclash.config.auto_restart='1'
-set openclash.config.auto_restart_day_time='3'
-set openclash.config.enable_dns='1'
-set openclash.config.ipv6_dns='1'
-set openclash.config.append_wan_dns='0'
-set openclash.config.disable_udp_quic='0'
-set openclash.config.auto_update='1'
-set openclash.config.config_auto_update_mode='0'
-set openclash.config.config_update_week_time='*'
-
-set openclash.config.core_type='Meta'
-set openclash.config.enable_meta_core='1'
-set openclash.config.enable_meta_sniffer='1'
-set openclash.config.enable_meta_sniffer_custom='0'
-set openclash.config.common_ports='1'
-set openclash.config.append_default_dns '0'
-set openclash.config.enable_custom_dns '1'
-
-set network.lan.ipaddr='LAN_IP'
-commit
-EOF
+uci set network.lan.ipaddr='LAN_IP'
+uci commit network
 
 mkdir -p /etc/dnsmasq.d/
 echo "conf-dir=/etc/dnsmasq.d/" >> /etc/dnsmasq.conf
