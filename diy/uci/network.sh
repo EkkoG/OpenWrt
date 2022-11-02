53d236b5ad30ec3558c141ddbdd3da619aee9bed
EkkoG <beijiu572@gmail.com>
2022-11-02 23:41:56 +0800

    Revert "Revert "Revert "use uci batch syntax"""
    This reverts commit 31f4f7de81ced9ef0f41ad4217fd76d30dff27b5.

--- diy/uci/network.sh
+++ diy/uci/network.sh
@@ -1,6 +1,4 @@
-uci batch << EOF
-set network.wan.proto='pppoe'
-set network.wan.username='PPPOE_USERNAME'
-set network.wan.password='PPPOE_PASSWORD'
-commit
-EOF
+uci set network.wan.proto='pppoe'
+uci set network.wan.username='PPPOE_USERNAME'
+uci set network.wan.password='PPPOE_PASSWORD'
+uci commit network
