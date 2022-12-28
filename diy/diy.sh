50fa23954db72bdd1fb4d8ab5964c82a4b8eb6f9
EkkoG <beijiu572@gmail.com>
2022-12-28 23:19:43 +0800

    refine code

--- diy/diy.sh
+++ diy/diy.sh
@@ -1,16 +1,13 @@
 #!/bin/bash -e
 
-IPK_ARCH=
-IPK_ARCH_STYLE_2=
-CLASH_ARCH=
 CLASH_META_VERSION=1.13.2
 if [ "$TARGET" = "x86_64" ]; then
     IPK_ARCH=x86_64
-    IPK_ARCH_STYLE_2=x86/64
+    IPK_ARCH_2=x86/64
     CLASH_ARCH=amd64-compatible
 elif [ "$TARGET" = "rockchip" ]; then
     IPK_ARCH=aarch64_generic
-    IPK_ARCH_STYLE_2=rockchip/armv8
+    IPK_ARCH_2=rockchip/armv8
     CLASH_ARCH=arm64
 fi
 
@@ -18,7 +15,7 @@ fi
 
 # 添加软件源
 THIRD_SOURCE=$(cat <<-END
-src/gz simonsmh https://ghproxy.com/https://github.com/simonsmh/openwrt-dist/blob/packages/${IPK_ARCH_STYLE_2}
+src/gz simonsmh https://ghproxy.com/https://github.com/simonsmh/openwrt-dist/blob/packages/${IPK_ARCH_2}
 src/gz ekkog https://ghproxy.com/https://github.com/ekkog/openwrt-dist/blob/packages/${IPK_ARCH}-21.02.5
 END
 
