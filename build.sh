3a21aa1d9acedb7e564c4a619b4111ad386dc221
EkkoG <beijiu572@gmail.com>
2023-09-13 00:09:09 +0800

    Fixed feed

--- build.sh
+++ build.sh
@@ -21,7 +21,60 @@ OPENWRT_VERSION=$(cat ./include/version.mk | grep 'VERSION_NUMBER:=$(if' | awk -
 BIG_VERSION=$(echo $OPENWRT_VERSION | awk -F '.' '{print $1"."$2}')
 
 echo "PACKAGES_ARCH: $PACKAGES_ARCH OPENWRT_VERSION: $OPENWRT_VERSION BIG_VERSION: $BIG_VERSION"
+DISTRIB_ARCH=$PACKAGES_ARCH
+DISTRIB_RELEASE=$OPENWRT_VERSION
+add_packages() {
+    feed=$1
 
+    all_supported=$(curl https://sourceforge.net/projects/ekko-openwrt-dist/files/$feed/ | grep -e "<th.*files/$feed" | grep -o 'href="/projects[^"]*"' | sed 's/href="//' | sed 's/"$//' | awk -F/ '{print $6}')
+    echo "All supported version: "
+    echo "$all_supported"
+
+    version=$(echo "$DISTRIB_RELEASE" | awk -F- '{print $1}')
+    echo "Current version: $version"
+
+    if [ "$feed" == "luci" ]; then
+        supported=$(echo "$all_supported" | grep $version)
+        feed_version="$DISTRIB_RELEASE"
+    else
+        supported=$(echo "$all_supported" | grep $DISTRIB_ARCH | grep $version)
+        feed_version="$DISTRIB_ARCH-v$DISTRIB_RELEASE"
+    fi
+
+    echo "Supported version: "
+    echo "$supported"
+
+    if [ -z "$supported" ]; then
+        echo "Your device is not supported"
+        exit 1
+    fi
+
+    full_support=0
+    for i in $supported; do
+        if [ "$i" = "$feed_version" ]; then
+            full_support=1
+            break
+        fi
+    done
+
+    if [ "$full_support" = "0" ]; then
+        echo "Your device is not fully supported"
+        echo "Find the latest version that supports your device"
+
+        # 过滤掉 rc 和 SNAPSHOT 版本, 不用 grep
+        final_release=$(echo "$supported" | grep -v "\-rc" | grep -v "SNAPSHOT" | tail -n 1)
+        if [ -z "$final_release" ]; then
+            echo "No final release found, use the latest rc version"
+            feed_version=$(echo "$supported" | grep "\-rc" | tail -n 1)
+        else
+            feed_version=$final_release
+        fi
+    fi
+    echo "Feed version: $feed_version"
+    EKKOG_FEED="src/gz ekkog_$feed https://ghproxy.imciel.com/https://downloads.sourceforge.net/project/ekko-openwrt-dist/$feed/$feed_version"
+    # 添加软件源到第一行
+    echo "$EKKOG_FEED" | cat - ./repositories.conf > temp && mv temp ./repositories.conf
+}
 
 if [[ $OPENWRT_VERSION =~ "SNAPSHOT" ]]; then
 EKKOG_FEED=$(cat <<-END
@@ -57,8 +110,11 @@ fi
 if [ $USE_MIRROR = '1' ]; then
     sed -i 's/https:\/\/downloads.'"$PROJECT_NAME"'.org/https:\/\/mirrors.pku.edu.cn\/'"$PROJECT_NAME"'/g' ./repositories.conf
 fi
-# 添加软件源
-echo "$EKKOG_FEED" >> ./repositories.conf
+
+add_packages "luci"
+add_packages "packages"
+add_packages "dae"
+add_packages "clash"
 
 mkdir -p files/etc/opkg/
 echo "$EKKOG_FEED" >> files/etc/opkg/customfeeds.conf
@@ -99,7 +155,7 @@ if [ $PROXY_CLIENT = "openclash" ]; then
     all_packages="$all_packages luci-app-openclash clash-meta-for-openclash"
 
     mkdir -p files/etc/openclash/config
-    wget $CLASH_CONFIG_URL -O files/etc/openclash/config/config.yaml
+    wget --user-agent='clash' $CLASH_CONFIG_URL -O files/etc/openclash/config/config.yaml
 
 elif [ $PROXY_CLIENT = "passwall" ]; then
     all_packages="$all_packages luci-app-passwall luci-i18n-passwall-zh-cn"
