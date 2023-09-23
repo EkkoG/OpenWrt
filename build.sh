73cb0ca1e3c2d77f681ba8410c3aa762c6846e05
EkkoG <beijiu572@gmail.com>
2023-09-23 20:05:03 +0800

    Fixed feed url

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -e
 if [[ $PWD =~ "immortalwrt" ]]; then
     PROJECT_NAME="immortalwrt"
+    sudo chown -R $(whoami):$(whoami) bin
 else
     PROJECT_NAME="openwrt"
+    chown -R $(whoami):$(whoami) bin
 fi
 
 
--- run.sh
+++ run.sh
@@ -58,14 +58,11 @@ if [[ $IMAGEBUILDER_IMAGE =~ "immortalwrt" ]]; then
 else
     BUILD_DIR=/builder
 fi
-uid=$(id -u)
-gid=$(id -g)
 docker_compose_file_content=$(cat <<-END
 version: "3.5"
 services:
   imagebuilder:
     image: "$IMAGEBUILDER_IMAGE"
-    user: "$uid:$gid"
     container_name: imagebuilder
     environment:
       - PROFILE=$PROFILE
@@ -73,7 +70,7 @@ services:
     env_file:
       - ./.env
     volumes:
-      - ./bin:$BUILD_DIR/bin:z
+      - ./bin:$BUILD_DIR/bin
       - ./build.sh:$BUILD_DIR/build.sh
       - ./files:$BUILD_DIR/custom_files
       - ./.env:$BUILD_DIR/.env
