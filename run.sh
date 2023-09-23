a6de3c4846e26dd1deb72bec90a7eca42ec2bf0e
EkkoG <beijiu572@gmail.com>
2023-09-23 22:57:40 +0800

    Try

--- run.sh
+++ run.sh
@@ -113,7 +113,7 @@ if [ ! -z $RM_FIRST ]; then
     compose rm -f
 fi
 
-# mkdir -p bin
+mkdir -p bin
 # # macOS no need to change the owner
 # # change the owner of bin to 1000:1000 when running on linux
 # if [[ $(uname) =~ "Linux" ]]; then
