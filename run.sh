97690cdd14dcb5f8f6d1c15ad447ce4142c02dba
EkkoG <beijiu572@gmail.com>
2023-09-23 21:21:01 +0800

    Try

--- run.sh
+++ run.sh
@@ -106,6 +106,13 @@ if [ ! -z $RM_FIRST ]; then
     compose rm -f
 fi
 
+mkdir -p bin
+# macOS no need to change the owner
+# change the owner of bin to 1000:1000 when running on linux
+if [[ $(uname) =~ "Linux" ]]; then
+    chown -R 1000:1000 bin
+fi
+
 compose up --remove-orphans
 build_status=$?
 compose rm -f
