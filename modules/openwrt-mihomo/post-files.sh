244d677d3a7ff612283f087f9a9cc0a5506fc9d2
Ekko <beijiu572@gmail.com>
2024-10-19 00:29:03 +0800

    feat: add OpenWrt-mihomo module only base install config, no default run config

---
+++ modules/openwrt-mihomo/post-files.sh
@@ -0,0 +1,10 @@
+
+PACKAGES_ARCH=$(cat .config | grep CONFIG_TARGET_ARCH_PACKAGES | awk -F '=' '{print $2}' | sed 's/"//g')
+
+feed="src/gz morytyann_mihomo https://mirror.ghproxy.com/https://github.com/morytyann/OpenWrt-mihomo/blob/gh-pages/$PACKAGES_ARCH/mihomo"
+mkdir -p files/etc/opkg/
+echo "$feed" >> files/etc/opkg/customfeeds.conf
+# 添加软件源到第一行
+echo "$feed" | cat - ./repositories.conf > temp && mv temp ./repositories.conf
+
+cp files/etc/opkg/keys/* keys
